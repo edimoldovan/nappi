67e69a34e15bf1bf0cb0ed95522b1221cd89e5f6
Moldován Eduárd <edimoldovan@gmail.com>
2014-11-22 02:20:33 +0100

    Fix indentation

--- app.js
+++ app.js
@@ -5,83 +5,83 @@
 /*global module, process,EventEmitter,__dirname,require,$,console,cookie,alert,Ember,jQuery,FileReader,canvas,FB*/
 
 var env = process.env.NODE_ENV || "development",
-	http = require("http"),
-	url = require("url"),
-	config = require("_/configs/" + env + ".json"),
-	database = require("_/models/mongo") ();
+  http = require("http"),
+  url = require("url"),
+  config = require("_/configs/" + env + ".json"),
+  database = require("_/models/mongo") ();
 
 
 function write404(res) {
-	"use strict";
-	res.writeHead(404, {"Content-Type": "application/json; charset=utf-8"});
-	res.end();
+  "use strict";
+  res.writeHead(404, {"Content-Type": "application/json; charset=utf-8"});
+  res.end();
 }
 
 http.createServer(function (req, res) {
-	"use strict";
-	var parsedUrl = url.parse(req.url, true),
-			splitUrl = parsedUrl.pathname.replace(/^\/|\/$/g, "").split("/"),
-			namespace = splitUrl[0],
-			id = splitUrl[1],
-			output = {},
-			method = req.method.toLowerCase(),
-			namespaceConfig,
-			applyParams = [],
-			handler,
-			body = "";
+  "use strict";
+  var parsedUrl = url.parse(req.url, true),
+      splitUrl = parsedUrl.pathname.replace(/^\/|\/$/g, "").split("/"),
+      namespace = splitUrl[0],
+      id = splitUrl[1],
+      output = {},
+      method = req.method.toLowerCase(),
+      namespaceConfig,
+      applyParams = [],
+      handler,
+      body = "";
 
-	console.log(new Date().toString() + " [" + method + "] [" + env + "] " + req.url);
+  console.log(new Date().toString() + " [" + method + "] [" + env + "] " + req.url);
 
-	function handleResults(results) {
-		output[namespaceConfig.plural] = results;
-		res.end(JSON.stringify(output));
-	}
+  function handleResults(results) {
+    output[namespaceConfig.plural] = results;
+    res.end(JSON.stringify(output));
+  }
 
-	if (!config.api.namespaces[namespace] || !namespace) {
-		write404(res);
-	}
+  if (!config.api.namespaces[namespace] || !namespace) {
+    write404(res);
+  }
 
-	namespaceConfig = config.api.namespaces[namespace];
+  namespaceConfig = config.api.namespaces[namespace];
 
-	if (!namespaceConfig[method]) {
-		write404(res);
-	}
+  if (!namespaceConfig[method]) {
+    write404(res);
+  }
 
-	applyParams.push(namespace);
+  applyParams.push(namespace);
 
-	req.on("data", function (chunk) {
-		body += chunk;
-	});
-	req.on("end", function () {
+  req.on("data", function (chunk) {
+    body += chunk;
+  });
+  req.on("end", function () {
 
-		if (method === "get") {
-			handler = database;
-			if (id) {
-				method = "getOne";
-				applyParams.push(id);
-			} else {
-				applyParams.push({});
-				applyParams.push(100000);
-			}
-		} else if (method === "post") {
-			applyParams.push(JSON.parse(body)[namespaceConfig.singular]);
-		} else if (method === "put") {
-			applyParams.push(id);
-			applyParams.push(JSON.parse(body)[namespaceConfig.singular]);
-		} else if (method === "delete") {
-			applyParams.push(id);
-		}
+    if (method === "get") {
+      handler = database;
+      if (id) {
+        method = "getOne";
+        applyParams.push(id);
+      } else {
+        applyParams.push({});
+        applyParams.push(100000);
+      }
+    } else if (method === "post") {
+      applyParams.push(JSON.parse(body)[namespaceConfig.singular]);
+    } else if (method === "put") {
+      applyParams.push(id);
+      applyParams.push(JSON.parse(body)[namespaceConfig.singular]);
+    } else if (method === "delete") {
+      applyParams.push(id);
+    }
 
-		if (namespaceConfig[method].controller) {
-			// handler = controller;
-			console.log("controller");
-		} else {
-			handler = database;
-		}
+    if (namespaceConfig[method].controller) {
+      // handler = controller;
+      console.log("controller");
+    } else {
+      handler = database;
+    }
 
-		applyParams.push(handleResults);
-		handler[method].apply(this, applyParams);
-	});
+    applyParams.push(handleResults);
+    handler[method].apply(this, applyParams);
+  });
 
 }).listen(3000);
 
